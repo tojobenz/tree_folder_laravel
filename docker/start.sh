6567b92b05276af2a8d8a075c4060b579ecae523
Aina Tojosoa <ainatojosoa@gmail.com>
2026-09-10 18:18:52 +0200

    fix jwt 2

--- docker/start.sh
+++ docker/start.sh
@@ -21,8 +21,6 @@ QUEUE_CONNECTION=${QUEUE_CONNECTION:-sync}
 
 ADMINEMAIL=${ADMINEMAIL:-admin@example.com}
 SOCIETENAME="${SOCIETENAME:-Ma Société}"
-
-JWT_SECRET=${JWT_SECRET:-$(openssl rand -base64 64)}
 EOF
 
 # Create SQLite database if it doesn't exist
